122cdba34afac7080a6d11b43382507c3c005220
Talita Fraga <talitafraga@MacBook-Air-de-Talita.local>
2025-05-21 21:16:52 -0300

    refactor: dashboard e sidebar

--- front/loja-maquiagem/src/App.js
+++ front/loja-maquiagem/src/App.js
@@ -14,26 +14,33 @@ import ListarClientes from './components/ListarClientes';
 import ListarFuncionarios from './components/ListarFuncionarios';
 import ListaFornecedores from './components/ListaFornecedores';
 import EditarFornecedor from './components/EditarFornecedor';
+import Sidebar from '../src/pages/SideBar';
+
 
 
 function App() {
   return (
     <Router>
+      <Sidebar>
+        <Routes>
+          <Route path="/lista" element={<ListaPessoas />} />
+          <Route path="/cadastro-pessoa" element={<CadastrarPessoa />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/vincular" element={<VincularPessoa />} />
+          <Route path="/produtos" element={<CadastrarProduto />} />
+          <Route path="/fornecedor" element={<CadastrarFornecedor/>}/>
+          <Route path="/pessoas/:cpf" element={<EditarPessoa/>}/>
+          <Route path="/clientes" element={<ListarClientes />} />
+          <Route path="/funcionarios" element={<ListarFuncionarios />} />
+          <Route path="/lista-fornecedores" element={<ListaFornecedores />} />
+          <Route path="/editar-fornecedor/:cnpj" element={<EditarFornecedor />} />
+          <Route path="/dashboard" element={<Dashboard/>}/>
+        </Routes>
+      </Sidebar>
+
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path="/lista" element={<ListaPessoas />} />
-        <Route path="/cadastro-pessoa" element={<CadastrarPessoa />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/vincular" element={<VincularPessoa />} />
-        <Route path="/produtos" element={<CadastrarProduto />} />
-        <Route path="/fornecedor" element={<CadastrarFornecedor/>}/>
-        <Route path="/dashboard" element={<Dashboard/>}/>
         <Route path="/dashboard-vendedor" element={<DashboardVendedor/>}/>
-        <Route path="/pessoas/:cpf" element={<EditarPessoa/>}/>
-        <Route path="/clientes" element={<ListarClientes />} />
-        <Route path="/funcionarios" element={<ListarFuncionarios />} />
-        <Route path="/lista-fornecedores" element={<ListaFornecedores />} />
-        <Route path="/editar-fornecedor/:cnpj" element={<EditarFornecedor />} />
 
       </Routes>
     </Router>
